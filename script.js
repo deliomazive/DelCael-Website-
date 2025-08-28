3201fb4340e3919f982f807975517e3b37555acf
deliomazive <deliomazivea12@gmail.com>
2025-08-28 02:32:53 +0200

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// Arquivo reservado para futuras interações
+function goToSecondPage() {
+  window.location.href = "aprendizagem.html";
+}
+
+function goToWhatsApp() {
+  window.open("https://wa.me/258XXXXXXXXX", "_blank");
+}
